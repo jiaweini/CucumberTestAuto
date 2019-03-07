d24575d6198e25637fdc7ae5edb99c9510dcfcd9
Jiawei Ni <jiaweini@live.com>
2019-03-07 17:48:49 -0500

    emailTest project created

---
+++ emailTest/src/emailTest/seleniumScript.java
@@ -0,0 +1,27 @@
+package emailTest;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class seleniumScript {
+	public static void main(String[] args) throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium-java-edureka\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://login.yahoo.com/");
+		driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("edureka@yahoo.com");
+		
+		
+	}
+}
+
+
+ 

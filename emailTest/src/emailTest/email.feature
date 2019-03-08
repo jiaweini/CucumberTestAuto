8eeaafdf633cfe3130ee11eb923a03672c8d59b9
Jiawei Ni <jiaweini@live.com>
2019-03-08 14:56:18 -0500

    switched to gmail

--- emailTest/src/emailTest/email.feature
+++ emailTest/src/emailTest/email.feature
@@ -1,1 +1,16 @@
 https://www.guru99.com/upload-download-file-selenium-webdriver.html
+
+Story: 
+As an Email user,
+I would like to be able to send an email with an image attached
+so the receiver could receive the image
+
+
+Normal Flow:
+Sending email with image attached to an address
+
+    Given I am logged in on a new email page 
+    When I fill an email address under “To”
+    And I drag an image into the page
+    And I press “Send”
+    Then the email is sent
--- emailTest/src/emailTest/seleniumScript.java
+++ emailTest/src/emailTest/seleniumScript.java
@@ -9,9 +9,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class seleniumScript {
-    private WebDriver driver;
+    private static WebDriver driver;
 
-    private final static String SEND_EMAIL_URL = "https://outlook.office.com/mail/deeplink/compose?popoutv2=1";
+    private final static String SEND_EMAIL_URL = "https://mail.google.com/mail/u/0/#inbox?compose=new";
     private final String SEND_BTN = "send";
 
     
@@ -19,7 +19,6 @@ public class seleniumScript {
     public static void givenOnSendEmailPage() throws Throwable {
     	System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
@@ -27,23 +26,44 @@ public class seleniumScript {
 		//   -get on login page
 		driver.get(SEND_EMAIL_URL);
 		//   -enter email and click next
-		driver.findElement(By.xpath("//input[@id='i0116']")).sendKeys("jiawei.ni@mail.mcgill.ca");
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("jiaweini0@gmail.com");
 		WebElement nextBtn = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.elementToBeClickable(By.id("idSIButton9")));
+                .until(ExpectedConditions.elementToBeClickable(By.id("identifierNext")));
         nextBtn.click();
     	//   -enter password and click
-        driver.findElement(By.xpath("//input[@id='passwordInput']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("1234");
         WebElement signInBtn = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.elementToBeClickable(By.id("submitButton")));
+                .until(ExpectedConditions.elementToBeClickable(By.id("passwordNext")));
         signInBtn.click();
-        //   -stay signed in? yes
-        WebElement staySignInBtn = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.elementToBeClickable(By.id("idSIButton9")));
-        staySignInBtn.click();
         
+        //   -go to send email page
+        //driver.get(SEND_EMAIL_URL);
+        WebElement composeBtn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div")));
+        composeBtn.click();
     }
     
     
+    //Given("^When I fill an email address under “To”$")
+    public static void whenFillTo() throws Throwable {
+    	
+    
+    	
+    	
+    	driver.findElement(By.className("_2BCZP_W9VLRv-NN3SC1nnS fxwJwkd4_ngPI8kgzfk88 _7fiIAr1gtfi5q48-tfia-")).sendKeys("jiaweini@live.com");
+    	//ms-BasePicker-input pickerInput_269bfa71
+    	
+    	
+    	
+    	
+    	
+    	
+    	//WebElement sendBtn = (new WebDriverWait(driver, 10))
+        //        .until(ExpectedConditions.elementToBeClickable(By.name("Attach")));
+        //sendBtn.click();
+    	
+    	
+    }
     
     
     
@@ -51,6 +71,8 @@ public class seleniumScript {
     
 	public static void main(String[] args) throws Throwable{
 		givenOnSendEmailPage();
+		whenFillTo();
+		
 		/*
 		WebElement btn = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.elementToBeClickable(By.id("send")));
